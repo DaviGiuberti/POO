dc72d8328edf51c1240873999d796cc6fa851905
PauloCrot <pauloramoscrot1@gmail.com>
2026-06-03 19:35:31 -0300

    feat: ambiente configurado e testes de compra rodando com sucesso

--- ItemDigital.ts
+++ ItemDigital.ts
@@ -5,11 +5,13 @@ export abstract class ItemDigital {
         protected precoBase: number
     ) {}
 
-
-    public getID(): string {return this.id; }
-    public getTitulo(): string {return this.titulo; }
+    public getID(): string { 
+        return this.id; 
+    }
+    
+    public getTitulo(): string { 
+        return this.titulo; 
+    }
 
     public abstract calcularPrecoFinal(): number;
-
 }
-
